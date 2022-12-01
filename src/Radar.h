b62735b88050607f05c05eb3d2c76900af9a9cc6
Zachary <z_zazzar@live.concordia.ca>
2022-12-01 18:56:41 -0500

    Initial Commit to branch.

--- src/Radar.h
+++ src/Radar.h
@@ -6,6 +6,7 @@
 #include "Coordinates.h"
 #include "Velocity.h"
 #include "Airspace.h"
+#include "Aircraft.h"
 #include <pthread.h>
 
 using namespace std;
@@ -18,7 +19,11 @@ public:
 	virtual ~Radar();
 	pthread_t run();
 
-
+	//lets declare some constants that will probably  be useful
+	const int static MIN_RADAR_HEIGHT = 15000;
+	const int static MAX_RADAR_HEIGHT = 40000; //Min height + height of flight zone (25000)
+	const int static MIN_SAFE_ALTITUDE = 15000; //Same as minimum height of tracking area
+	
 private:
 	pthread_attr_t* tAttr;
 	pthread_mutex_t r_mutex;
